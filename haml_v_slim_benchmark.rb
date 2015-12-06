0532d0e4ed3e70910a41e956dab4f2ba9174e096
Maciej Kalisz <maciej.d.kalisz@gmail.com>
2015-12-06 18:45:04 +0100

    Refactor benchmark

--- haml_v_slim_benchmark.rb
+++ haml_v_slim_benchmark.rb
@@ -4,102 +4,101 @@ require 'tilt'
 require 'benchmark'
 require 'action_view'
 include Benchmark
+include ActionView::Helpers
+include ActionView::RoutingUrlFor
 
-puts "\n7 TESTS, 100_000 ITERATIONS FOR EACH FILE (HTML, HAML, SLIM)\n"
+# SETUP
 
-def round_caption(round)
-  [
-    ' EMPTY FILES',
-    ' FILES WITH STATIC CONTENT',
-    ' + BOOTSTRAP CSS FROM LOCAL SSD',
-    ' + BOOTSTRAP JS AND JQUERY FROM LOCAL SSD'
-  ][round]
+def title(round, caption)
+  "\n#{round}. #{caption}\n#{' ' * 10}#{CAPTION}"
 end
 
-for i in 1..4 do # rubocop:disable Style/For
-
-  html = Tilt.new("views/#{i}.html")
-  haml = Tilt.new("views/#{i}.haml")
-  slim = Tilt.new("views/#{i}.slim")
-
-  title = "\n#{i}. #{round_caption(i - 1)}\n#{' ' * 10}#{CAPTION}"
-  Benchmark.benchmark(title, 10, FORMAT) do |x|
-    x.report("#{i}html:") do
-      100_000.times { html.render }
-    end
-
-    x.report("#{i}haml:") do
-      100_000.times { haml.render }
-    end
-
-    x.report("#{i}slim:") do
-      100_000.times { slim.render }
-    end
+class Person
+  attr_reader :first_name, :last_name, :age
+  def initialize(options)
+    @first_name = options[:first_name]
+    @last_name = options[:last_name]
+    @age = options[:age]
   end
 end
 
-# ROUND 5: + ruby for and while loops, 2 x 1000
+person = Person.new(first_name: 'Tomek', last_name: 'Nowak', age: 30)
+
+html1 = Tilt.new('views/1.html')
+haml1 = Tilt.new('views/1.haml')
+slim1 = Tilt.new('views/1.slim')
+
+html2 = Tilt.new('views/2.html')
+haml2 = Tilt.new('views/2.haml')
+slim2 = Tilt.new('views/2.slim')
+
+haml3 = Tilt.new('views/3.haml')
+slim3 = Tilt.new('views/3.slim')
+
+haml4 = Tilt.new('views/4.haml')
+slim4 = Tilt.new('views/4.slim')
 
 haml5 = Tilt.new('views/5.haml')
 slim5 = Tilt.new('views/5.slim')
 
-# ROUND 6: + variables
+# BENCHMARKS
 
-class Person
-  attr_reader :first_name, :last_name, :age
-  def initialize(first_name, last_name, age)
-    @first_name = first_name
-    @last_name = last_name
-    @age = age
+puts "\n4 TESTS, 100_000 ITERATIONS EACH\n"
+
+Benchmark.benchmark(title(1, 'EMPTY FILES'), 10, FORMAT) do |x|
+  x.report('1html:') do
+    100_000.times { html1.render }
+  end
+
+  x.report('1haml:') do
+    100_000.times { haml1.render }
+  end
+
+  x.report('1slim:') do
+    100_000.times { slim1.render }
   end
 end
 
-# rubocop:disable Lint/UselessAssignment
-person = Person.new(first_name = 'Tomek', last_name = 'Nowak', age = 30)
-# rubocop:enable Lint/UselessAssignment
+Benchmark.benchmark(title(2, 'FILES WITH STATIC CONTENT'), 10, FORMAT) do |x|
+  x.report('2html:') do
+    100_000.times { html2.render }
+  end
 
-haml6 = Tilt.new('views/6.haml')
-slim6 = Tilt.new('views/6.slim')
+  x.report('2haml:') do
+    100_000.times { haml2.render }
+  end
 
-# ROUND 7: + actionview helpers
+  x.report('2slim:') do
+    100_000.times { slim2.render }
+  end
+end
 
-include ActionView::Helpers
-include ActionView::RoutingUrlFor
+Benchmark.benchmark(title(3, 'RUBY FOR AND WHILE LOOPS, 2 x 100'), 10, FORMAT) do |x|
+  x.report('3haml') do
+    100_000.times { haml3.render }
+  end
 
-haml7 = Tilt.new('views/7.haml')
-slim7 = Tilt.new('views/7.slim')
+  x.report('3slim') do
+    100_000.times { slim3.render }
+  end
+end
 
-#
+Benchmark.benchmark(title(4, 'ACTIONVIEW HELPERS'), 10, FORMAT) do |x|
+  x.report('4haml') do
+    100_000.times { haml4.render }
+  end
 
-title = "\n5. + RUBY FOR AND WHILE LOOPS, 2 x 1000\n#{' ' * 10}#{CAPTION}"
-Benchmark.benchmark(title, 10, FORMAT) do |x|
+  x.report('4slim') do
+    100_000.times { slim4.render }
+  end
+end
+
+Benchmark.benchmark(title(5, 'VARIABLES'), 10, FORMAT) do |x|
   x.report('5haml') do
-    100_000.times { haml5.render }
+    100_000.times { haml5.render(person, first_name: 'Tomek', last_name: 'Kowal', age: 30) }
   end
 
   x.report('5slim') do
-    100_000.times { slim5.render }
-  end
-end
-
-title = "\n6. + VARIABLES\n#{' ' * 10}#{CAPTION}"
-Benchmark.benchmark(title, 10, FORMAT) do |x|
-  x.report('6haml') do
-    100_000.times { haml6.render(person, first_name: 'Tomek', last_name: 'Kowal', age: 30) }
-  end
-
-  x.report('6slim') do
-    100_000.times { slim6.render(person, first_name: 'Tomek', last_name: 'Kowal', age: 30) }
-  end
-end
-
-title = "\n7. + ACTIONVIEW HELPERS\n#{' ' * 10}#{CAPTION}"
-Benchmark.benchmark(title, 10, FORMAT) do |x|
-  x.report('7haml') do
-    100_000.times { haml7.render }
-  end
-
-  x.report('7slim') do
-    100_000.times { slim7.render }
+    100_000.times { slim5.render(person, first_name: 'Tomek', last_name: 'Kowal', age: 30) }
   end
 end
